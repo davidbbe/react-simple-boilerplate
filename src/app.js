ff076569f79149585c5921a5b1995f44bdfa46cc
David Beauchamp <david123beauchamp@gmail.com>
2018-04-26 13:18:37 -0600

    init commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<p>Boilerplate</p>, document.getElementById('app'));
